abb847a48f62bddfeeacf149fe10d781552a8e8a
Akhzar Farhan <atakhzar@gmail.com>
2019-05-02 12:11:12 +0530

    initial build

---
+++ precache-manifest.04269da5e925208e59e097b5a3e1e8a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0b9de3050c41b73cda67",
+    "url": "/static/js/main.dcf80efb.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "0b9de3050c41b73cda67",
+    "url": "/static/css/main.2064096f.chunk.css"
+  },
+  {
+    "revision": "11c230b426c653ec9a973f2deb78f586",
+    "url": "/index.html"
+  }
+];
